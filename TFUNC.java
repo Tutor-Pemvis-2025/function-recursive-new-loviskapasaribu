190356a45a9a8507fbe2a4165c2ced439730e575
loviskapasaribu <loviskapasaribu@gmail.com>
2025-11-11 20:56:32 +0700

    LOVISKA
    SELESAI

--- TFUNC.java
+++ TFUNC.java
@@ -3,7 +3,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class tutor_recurtion {
+public class TFUNC {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
