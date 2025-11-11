6fefbb529f1b5fd76071ff4dcc07a0ca3daa3665
loviskapasaribu <loviskapasaribu@gmail.com>
2025-11-11 20:53:29 +0700

    LOVISKA
    SELESAI

---
+++ TFUNC.java
@@ -0,0 +1,28 @@
+// 12S25016_Loviska Astria Pasaribu
+
+import java.util.*;
+import java.lang.Math;
+
+public class tutor_recurtion {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n, hasil;
+
+        n = Integer.parseInt(input.nextLine());
+        hasil = pertambahan(n);
+        System.out.println(hasil);
+    }
+    
+    public static int pertambahan(int n) {
+        int hasil;
+
+        if (n == 1) {
+            hasil = 1;
+        } else {
+            hasil = pertambahan(n - 1) + n;
+        }
+        
+        return hasil;
+    }
+}
